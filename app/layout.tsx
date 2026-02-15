df24f16bc395f1d158eb926c4459980773c48295
Vitor Borman <vitorkborman@gmail.com>
2026-02-15 19:52:59 -0300

    pwa base

--- app/layout.tsx
+++ app/layout.tsx
@@ -15,6 +15,7 @@ export default function RootLayout({
       <head>
         <link rel="manifest" href="/manifest.json" />
         <meta name="theme-color" content="#391e2a" />
+        <meta name="apple-mobile-web-app-capable" content="yes" />
       </head>
       <body>{children}</body>
     </html>
