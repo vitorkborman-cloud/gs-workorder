ae824d182c8f7cce3a19d19302dd76c61c6eb1bd
Vitor Borman <vitorkborman@gmail.com>
2026-03-27 21:25:33 -0300

    oidjfigod

--- next.config.ts
+++ next.config.ts
@@ -1,5 +1,12 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {};
+const nextConfig: NextConfig = {
+  experimental: {
+    // @ts-ignore: Essa propriedade é experimental e necessária para o Chromium na Vercel
+    outputFileTracingIncludes: {
+      '/api/gerar-pdf': ['./node_modules/@sparticuz/chromium/bin/*'],
+    },
+  },
+};
 
 export default nextConfig;
