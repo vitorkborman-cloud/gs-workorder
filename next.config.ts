66757868053146d7e71357c698367dd259f2c5c8
Vitor Borman <vitorkborman@gmail.com>
2026-03-31 21:24:07 -0300

    iodfjiogdf

--- next.config.ts
+++ next.config.ts
@@ -1,12 +1,5 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
-  experimental: {
-    // @ts-ignore: Impede a Vercel de apagar o Chromium no build
-    outputFileTracingIncludes: {
-      '/api/gerar-pdf': ['./node_modules/@sparticuz/chromium/bin/*'],
-    },
-  },
-};
+const nextConfig: NextConfig = {};
 
 export default nextConfig;
