bb624014db02b42c95696522a75e5fe51f11d02e
Vitor Borman <vitorkborman@gmail.com>
2026-06-22 21:35:51 -0300

    oijdsigo

--- proxy.ts
+++ proxy.ts
@@ -3,7 +3,7 @@ import { NextResponse, type NextRequest } from "next/server";
 
 const PUBLIC_ROUTES = ["/login"];
 
-export async function middleware(request: NextRequest) {
+export async function proxy(request: NextRequest) {
   const { pathname } = request.nextUrl;
 
   // Deixa rotas públicas e assets passarem sem verificação
